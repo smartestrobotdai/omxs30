b3086337c4820d203d74171aae69ee9bcf3829ac
smartestrobotdai <smartestrobotdai@gmail.com>
2019-06-05 08:50:58 +0200

    src: removed prediction_period=2

--- src/stockwormmanager.py
+++ src/stockwormmanager.py
@@ -22,7 +22,7 @@ class StockWormManager:
       {'name': 'num_layers', 'type': 'discrete', 'domain': (1,2,3,4,5,6,7,8)},
       {'name': 'rnn_type', 'type': 'discrete', 'domain': (0,1,2)},
       {'name': 'learning_period', 'type': 'discrete', 'domain': (20,30,40)},
-      {'name': 'prediction_period', 'type': 'discrete', 'domain': (2,5,10,20)},
+      {'name': 'prediction_period', 'type': 'discrete', 'domain': (5,10,20)},
       {'name': 'n_repeats', 'type': 'discrete', 'domain': (1,3,5,10,20,30,40)},
       {'name': 'beta', 'type': 'discrete', 'domain': (99,)},
       {'name': 'ema', 'type': 'discrete', 'domain': (20,)},
