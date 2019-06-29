262b89bf6dab93467c359d7b5d60a153e7d9eaf1
smartestrobotdai <smartestrobotdai@gmail.com>
2019-06-29 14:22:28 +0800

    tool to migrate stockworm cache, add is_stateful column

---
+++ src/tools/stockworm_migrate.py
@@ -0,0 +1,25 @@
+import sys
+import os.path
+sys.path.append("../")
+from util import *
+from optimizeresult import OptimizeResult
+from pathlib import Path
+
+stock_data_path = get_stock_data_dir()
+
+# This is to get the directory that the program  
+# is currently running in. 
+
+for filename in Path(stock_data_path).glob('**/stockworm_cache.txt'):
+	print('migrating: {}'.format(filename))
+	optimize_result = OptimizeResult(result_column_index=-2)
+	optimize_result.load(filename)
+	if (optimize_result.get_n_columns() == 18):
+		continue
+		
+	assert(optimize_result.get_n_columns() == 17)
+	optimize_result.add_column(13, 1.0)
+	assert(optimize_result.get_n_columns() == 18)
+	optimize_result.save(filename)
+
+
