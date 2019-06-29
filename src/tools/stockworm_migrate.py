8700687510be2bce6cd40c8f22934fdab0cf48b4
smartestrobotdai <smartestrobotdai@gmail.com>
2019-06-29 14:26:00 +0800

    check if the file has already been migrated

--- src/tools/stockworm_migrate.py
+++ src/tools/stockworm_migrate.py
@@ -11,12 +11,14 @@ stock_data_path = get_stock_data_dir()
 # is currently running in. 
 
 for filename in Path(stock_data_path).glob('**/stockworm_cache.txt'):
-	print('migrating: {}'.format(filename))
+	
 	optimize_result = OptimizeResult(result_column_index=-2)
 	optimize_result.load(filename)
 	if (optimize_result.get_n_columns() == 18):
+		print("{} has been already migrated.".format(filename))
 		continue
-		
+
+	print('migrating {}'.format(filename))
 	assert(optimize_result.get_n_columns() == 17)
 	optimize_result.add_column(13, 1.0)
 	assert(optimize_result.get_n_columns() == 18)
