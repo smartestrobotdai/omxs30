bf4e9576c18200d270699ebd4464e103155d1dc6
smartestrobotdai <smartestrobotdai@gmail.com>
2019-07-04 07:54:13 +0800

    search-worms.sh: run an endless loop for searching worms.

---
+++ src/tools/search-worms.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -lt 3 ]; then
+  echo "usage: ./search-worms.sh stock_name start_day_index end_day_index"
+  exit -1
+fi
+
+RUN=1
+while [ 1 ]
+do
+  DATE=`date -u`
+  echo "$DATE Starting $RUN runs"
+  python3 ./search-worms.py $1 $2 $3 $4
+  echo "$DATE Finishing $RUN runs"
+  RUN=`expr $RUN + 1`
+done
