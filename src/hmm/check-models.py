44aa796bb5cd0293fb703347974baa099f4573b2
smartestrobotdai <smartestrobotdai@gmail.com>
2019-07-19 11:36:55 +0800

    print out the profit for stock

--- src/hmm/check-models.py
+++ src/hmm/check-models.py
@@ -1,7 +1,8 @@
 import sys
 import os.path
+import numpy as np
 sys.path.append("../")
-from util import get_stock_id_by_name
+from util import get_stock_id_by_name, get_npy_filename
 from optimizeresult import OptimizeResult
 from hmm_util import get_cache_filename
 
@@ -17,6 +18,23 @@ number = 5
 
 if len(sys.argv) == 5:
   number = int(sys.argv[4])
+
+# find out the total raise.
+npy_filename = get_npy_filename(stock_name, stock_id, 1, 99)
+data = np.load(npy_filename, allow_pickle=True)
+start_price = data[0,0,6]
+end_price = data[-1,-1,6]
+# column 6 is the price!
+n_days = data.shape[0]
+profit = 1
+for d in range(n_days):
+	price = data[d,:,6]
+	#print("start: {} - end: {}".format(price[0], price[-2]))
+	rate = price[-2] / price[0]
+	profit = profit * rate
+
+print("Stock profit w/o overnight: {}".format(profit))
+print("Stock profit w overnight: {}".format(end_price/start_price))
 
 cache_file = get_cache_filename(stock_name, start_day_index, end_day_index)
 worm_results = OptimizeResult(-1)
